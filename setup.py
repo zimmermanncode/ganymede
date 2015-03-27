9592137d7e6e99905b880f92ffd6247c871b025b
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-03-27 10:52:57 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,46 @@
+import sys
+import os
+
+from setuptools import setup
+
+ROOT = os.path.dirname(os.path.realpath(__file__))
+
+# trigger ganymede.coffee-->js compilation
+sys.path.insert(0, ROOT)
+import ganymede.static
+
+
+setup(
+  name='ganymede',
+  version=ganymede.__version__,
+  description="Hacking Jupyter's atmosphere",
+
+  author="Stefan Zimmermann",
+  author_email="<zimmermann.code@gmail.com>",
+  url="https://bitbucket.org/userzimmermann/ganymede",
+
+  license='LGPLv3',
+
+  install_requires=open(os.path.join(ROOT, 'requirements.txt')).read(),
+
+  packages=[
+    'ganymede',
+    'ganymede.static',
+    ],
+  package_data={
+    'ganymede.static': ['*.svg', '*.css', '*.js'],
+    },
+
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved'
+    ' :: GNU Library or Lesser General Public License (LGPL)',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development',
+    'Topic :: Utilities',
+    ],
+  keywords=[
+    'ganymede', 'jupyter', 'ipython', 'notebook',
+    ],
+  )
