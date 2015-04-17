e125a5c7d87d5e44e55fc31a2db19b40efe6429e
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-04-17 14:02:19 +0000

    setup: always import ganymede

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ ROOT = os.path.dirname(os.path.realpath(__file__))
 
 sys.path.insert(0, ROOT)
 
-
+import ganymede
 if os.path.exists(os.path.join(
   ROOT, 'ganymede', 'static', 'ganymede.coffee'
   )):
