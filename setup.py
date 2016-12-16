078170b2835091543bd78735a0518bc7474b2aab
Stefan Zimmermann <zimmermann.code@gmail.com>
2016-12-16 14:55:37 +0100

    setup: Update classifiers and code formatting

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 
 from setuptools import setup
 
+
 ROOT = os.path.dirname(os.path.realpath(__file__))
 
 sys.path.insert(0, ROOT)
@@ -16,35 +17,43 @@ if os.path.exists(os.path.join(
 
 
 setup(
-  name='ganymede',
-  version=ganymede.__version__,
-  description="Hacking Jupyter's atmosphere",
+    name='ganymede',
+    version=ganymede.__version__,
+    description="Hacking Jupyter's atmosphere",
 
-  author="Stefan Zimmermann",
-  author_email="<zimmermann.code@gmail.com>",
-  url="https://github.com/zimmermanncode/ganymede",
+    author="Stefan Zimmermann",
+    author_email="<zimmermann.code@gmail.com>",
+    url="https://github.com/zimmermanncode/ganymede",
 
-  license='GPLv3',
+    license='GPLv3',
 
-  install_requires=open(os.path.join(ROOT, 'requirements.txt')).read(),
+    install_requires=open(os.path.join(ROOT, 'requirements.txt')).read(),
 
-  packages=[
-    'ganymede',
-    'ganymede.static',
+    packages=[
+        'ganymede',
+        'ganymede.static',
     ],
-  package_data={
-    'ganymede.static': ['*.svg', '*.css', '*.js'],
+    package_data={
+        'ganymede.static': ['*.svg', '*.css', '*.js'],
     },
 
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: GNU General Public License (GPL)',
-    'Operating System :: OS Independent',
-    'Topic :: Software Development',
-    'Topic :: Utilities',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: JavaScript',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
     ],
-  keywords=[
-    'ganymede', 'jupyter', 'ipython', 'notebook',
+    keywords=[
+        'ganymede', 'jupyter', 'ipython', 'notebook',
     ],
-  )
+)
