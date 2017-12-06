afceacfccafa783598ca32e3ca94e1e4a3fed26a
Stefan Zimmermann <zimmermann.code@gmail.com>
2017-12-06 16:21:29 +0100

    setup: Add declarativewidgets packages
    --HG--
    branch : zimmermann

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-import sys
+from glob import glob
 import os
+import sys
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
@@ -18,6 +19,73 @@ if os.path.exists(os.path.join(
                 write_to='ganymede/__version__.py')
 
     import ganymede.static
+
+
+DECLARATIVEWIDGETS_ROOT = os.path.join(ROOT, 'jupyter-declarativewidgets')
+
+if os.path.exists(DECLARATIVEWIDGETS_ROOT):
+    # ==> trigger bower.json creation
+    import ganymede.static.urth
+
+
+PACKAGES = [
+    'ganymede',
+    'ganymede.static',
+    'ganymede.widgets',
+]
+
+
+DECLARATIVEWIDGETS_PACKAGE_ROOT = os.path.join(
+    DECLARATIVEWIDGETS_ROOT, 'kernel-python')
+
+PACKAGES.extend(find_packages(DECLARATIVEWIDGETS_PACKAGE_ROOT))
+
+PACKAGE_DIRS = {
+    pkg: os.path.relpath(
+        os.path.join(DECLARATIVEWIDGETS_PACKAGE_ROOT, pkg),
+        ROOT)
+    for pkg in ['declarativewidgets', 'urth']}
+
+
+PACKAGES.append('ganymede.widgets.ext')
+
+PACKAGE_DIRS['ganymede.widgets.ext'] = os.path.relpath(
+    os.path.join(DECLARATIVEWIDGETS_ROOT, 'nb-extension', 'python',
+                 'urth', 'widgets', 'ext'),
+    ROOT)
+
+
+STATIC_URTH = {
+    'ganymede.static.' + subpkg: os.path.relpath(
+        os.path.join(DECLARATIVEWIDGETS_ROOT, dirname),
+        ROOT)
+    for subpkg, dirname in [
+        ('urth',  'nb-extension'),
+        ('urth_components', 'elements'),
+    ]}
+
+PACKAGES.extend(STATIC_URTH)
+
+PACKAGE_DIRS.update(STATIC_URTH)
+
+
+def find_package_data(pkgdir, *patterns):
+    """
+    Recursively find files matching any of given `patterns` under `pkgdir`.
+
+    :return:
+       A list of all matching data file paths relative to given package dir
+    """
+    return [os.path.relpath(path, pkgdir) for pattern in patterns
+            for path in glob(os.path.join(pkgdir, '**', pattern),
+                             recursive=True)]
+
+
+PACKAGE_DATA = {
+    pkg: find_package_data(
+        PACKAGE_DIRS.get(pkg, pkg.replace('.', os.path.sep)),
+        '.*rc', '*.css', '*.html', '*.js', '*.json', '*.png', '*.svg')
+    for pkg in ['ganymede.static'] + list(STATIC_URTH)}
 
 
 setup(
@@ -39,13 +107,9 @@ setup(
         'write_to': 'ganymede/__version__.py',
     },
 
-    packages=[
-        'ganymede',
-        'ganymede.static',
-    ],
-    package_data={
-        'ganymede.static': ['*.svg', '*.css', '*.js'],
-    },
+    packages=PACKAGES,
+    package_dir=PACKAGE_DIRS,
+    package_data=PACKAGE_DATA,
 
     classifiers=[
         'Development Status :: 4 - Beta',
