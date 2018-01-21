2889f69e29927890a122b3c9580f610da68c7303
Stefan Zimmermann <zimmermann.code@gmail.com>
2018-01-21 20:08:50 +0100

    widgets: Check for None before Path() conversion
    --HG--
    branch : zimmermann

--- ganymede/widgets/__init__.py
+++ ganymede/widgets/__init__.py
@@ -29,9 +29,11 @@ from .ext.urth_import import get_nbextension_path
 
 
 #: The installed nbextentensions/declarativewidgets directory path
-DECLARATIVEWIDGETS_NBEXTENSION_PATH = Path(get_nbextension_path())
+DECLARATIVEWIDGETS_NBEXTENSION_PATH = get_nbextension_path()
 
-if DECLARATIVEWIDGETS_NBEXTENSION_PATH:
+if DECLARATIVEWIDGETS_NBEXTENSION_PATH is not None:
+    DECLARATIVEWIDGETS_NBEXTENSION_PATH = Path(
+        DECLARATIVEWIDGETS_NBEXTENSION_PATH)
     # ensure that all urth component dependencies are correctly installed
     with (DECLARATIVEWIDGETS_NBEXTENSION_PATH):
         nodely.bin['bower'](['install'])
