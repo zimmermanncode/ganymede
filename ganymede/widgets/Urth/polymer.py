9ffb8b7bbc90ab6a0196a3607d85ee14fa612678
Stefan Zimmermann <zimmermann.code@gmail.com>
2019-01-21 15:41:37 +0100

    Define basic Urth.PolymerComponent for deriving
    Avoids deriving meta members of Urth.Polymer
    
    --HG--
    branch : zimmermann

--- ganymede/widgets/Urth/polymer.py
+++ ganymede/widgets/Urth/polymer.py
@@ -5,7 +5,18 @@ Pythonic API for PolymerElements.
 from .component import Component
 
 
+class PolymerComponent(Component):
+
+    def __init__(self, name, html_class=None, **html_attrs):
+        super().__init__(
+            name, html_class=html_class,
+            package_owner='PolymerElements', version_spec='^1',
+            **html_attrs)
+
+
 class Meta(type(Component)):
+
+    Component = PolymerComponent
 
     def paper(cls, name, html_class=None, **html_attrs):
         return cls('paper-' + name, html_class=html_class, **html_attrs)
@@ -14,10 +25,5 @@ class Meta(type(Component)):
         return cls('iron-' + name, html_class=html_class, **html_attrs)
 
 
-class Polymer(Component, metaclass=Meta):
-
-    def __init__(self, name, html_class=None, **html_attrs):
-        super().__init__(name, html_class=html_class,
-                         package_owner='PolymerElements',
-                         version_spec='^1',
-                         **html_attrs)
+class Polymer(PolymerComponent, metaclass=Meta):
+    pass
